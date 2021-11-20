0dc53cb51254aea07a8702f158ffe3b7d71190d8
Razvan Filip <razvan.filip.70@gmail.com>
2021-11-20 23:44:02 +0200

    Removed GW2 module

--- raidManager/raidManager.py
+++ raidManager/raidManager.py
@@ -3,9 +3,7 @@ import asyncio
 import os
 import discord
 import datetime
-import random
 
-from gw2.gw2 import Gw2
 from raidManager.constants import NO_PLAYERS_SIGNED_STR
 from raidManager.form import Form
 from raidManager.raid import Raid
@@ -130,35 +128,6 @@ async def on_message(message):
     if message.content.startswith(KEY + " "):
 
         words = message.content.split()
-
-        ### GW2 ###
-        # Dailies
-        # i.e. "!yk dailies T4", "!yk dailies recommended"
-        if len(words) == 3:
-            words[1] = words[1].lower()
-            words[2] = words[2].upper()
-            accepted_words = ["T1", "T2", "T3", "T4", "RECOMMENDED"]
-            if (words[1] == "dailies" or words[1] == "daily") and words[2] in accepted_words:
-                tier = words[2]
-                gw2 = Gw2()
-                dailies = gw2.get_dailies(tier)
-
-                if dailies == False:
-                    await message.channel.send(author_id + " bad format. Try it like " + KEY + " dailies T4")
-                    return
-
-                else:
-                    today = str(datetime.datetime.today())
-                    today = str(today.split()[0])
-                    embedVar = discord.Embed(
-                        title="Fractal dailies of " + today, color=0x0066ff)
-                    frac_str = ""
-                    for daily in dailies:
-                        frac_str += ":cyclone: " + daily + "\n"
-                    embedVar.add_field(
-                        name="Daily fractals " + tier, value=frac_str, inline=False)
-                    await message.channel.send(embed=embedVar)
-                    return
 
         ### raid formular ###
         # i.e. !yk start raid
